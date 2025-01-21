f7663043c8cee8476fb4d5d15c03f4526e4550c7
Sri Lakshmi Polavarapu <srilakshmi.polavarapu01@gmail.com>
2025-01-20 21:58:04 -0800

    Flask is initialized, NLP setup is done, POST method is handled using generate_linkedin_post() method

--- backend/app.py
+++ backend/app.py
@@ -6,3 +6,20 @@ import nltk
 
 nltk.download('punkt')
 nltk.download('stopwords')
+
+#initializing Flask application by creating an instance of Flask class
+app = Flask(__name__)
+
+#NLP setup
+nlp = spacy.load("en_core_web_sm")
+
+#API Route
+@app.route("/generate_linkedin_post", methods=["POST"])
+
+#function to generate LinkedIn post, which handles POST request
+def generate_linkedin_post():
+    data = request.json
+    input_type = data.get('input_type')
+    content = data.get('content')
+    
+

03350e6de3f8b5e3fc6213839702c2942bf5a5e8
Sri Lakshmi Polavarapu <srilakshmi.polavarapu01@gmail.com>
2025-02-02 20:17:54 -0800

    the frontend part is going on - button click to generate summary is updated

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -27,3 +27,41 @@ def main():
         
     else:
         return "Error processing input"    
+    
+    #this section is for generating summary - button click
+    if st.button("Generate Summary"):
+        if input_type == "Enter Text" and not text.strip():
+            st.error("Please enter or paste your text before generating summary")
+        elif input_type == "Upload Text File" and not uploaded_file:
+            st.error("Please upload a text file before generating summary")
+        else:
+            with st.spinner("Generating summary..."):
+                try:
+                    if input_type == "Transcript":
+                        # Send text data to backend
+                        data = {"input_type": "Transcript", "content": transcript}
+                        response = requests.post(API_URL, json=data)
+
+                    elif input_type == "Text File":
+                        # Send file data to backend
+                        files = {"content": (uploaded_file.name, uploaded_file, "text/plain")}
+                        data = {"input_type": "Text File"}
+                        response = requests.post(API_URL, files=files, data=data)
+                    else:
+                        return "###"
+                    
+                    
+                    if response.status_code == 200:
+                        result = response.json()
+                        st.success("Summary Generated:")
+                        st.markdown("---")
+                        st.write(result["summary"])
+                        st.markdown("---")
+                    else:
+                        st.error("Error generating summary.")
+                        st.write(f"Details: {response.json().get('error', 'Unknown error')}")
+                except Exception as e:
+                    st.error("An unexpected error occurred while generating the summary.")
+                    st.write(f"Error details: {e}")
+                   
+            
